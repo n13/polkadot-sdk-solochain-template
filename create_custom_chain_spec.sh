fc54859c32e774203ddd861d5286293154ad53e1
NIK <nheger@gmail.com>
2025-01-29 15:06:00 +0800

    Create create_custom_chain_spec.sh

---
+++ create_custom_chain_spec.sh
@@ -0,0 +1,15 @@
+# this is needed so we have a chain spec to read from
+
+# first build the project then run this
+
+cargo build
+
+./target/release/solochain-template-node \
+  build-spec \
+  --chain dev \
+  --disable-default-bootnode > custom-spec.json
+
+  ./target/release/solochain-template-node \
+  build-spec \
+  --chain custom-spec.json \
+  --raw > custom-spec-raw.json
